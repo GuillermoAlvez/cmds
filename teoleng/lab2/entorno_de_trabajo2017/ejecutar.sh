ed1f64c5884417825ac48373addfb740f8fe54d9
GuillermoAlvez <guillermo_drumss@hotmail.com>
2017-06-15 15:14:07 -0300

    modificado:    jsonPython/parseJson.py 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/comparar.bat 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/comparar.sh 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/diff.exe 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/ejecutar.bat 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/ejecutar.sh 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/ejemplo.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test1.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test10.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test11.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test12.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test2.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test3.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test4.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test5.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test6.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test7.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test8.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/entradas/test9.in 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/grammar.txt 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/main.py 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/ejemplo.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test1.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test10.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test11.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test12.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test2.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test3.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test4.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test5.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test6.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test7.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test8.out 	nuevo archivo: teoleng/lab2/entorno_de_trabajo2017/salidas_oficiales/test9.out

---
+++ teoleng/lab2/entorno_de_trabajo2017/ejecutar.sh
@@ -0,0 +1,14 @@
+python main.py entradas/ejemplo.in salidas/ejemplo.out
+python main.py entradas/test1.in salidas/test1.out
+python main.py entradas/test2.in salidas/test2.out
+python main.py entradas/test3.in salidas/test3.out
+python main.py entradas/test4.in salidas/test4.out
+python main.py entradas/test5.in salidas/test5.out
+python main.py entradas/test6.in salidas/test6.out
+python main.py entradas/test7.in salidas/test7.out
+python main.py entradas/test8.in salidas/test8.out
+python main.py entradas/test9.in salidas/test9.out
+python main.py entradas/test10.in salidas/test10.out
+python main.py entradas/test11.in salidas/test11.out
+python main.py entradas/test12.in salidas/test12.out
+
